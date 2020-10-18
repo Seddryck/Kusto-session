e70511d6113941962b033affb94cc683e1778765
Cédric L. Charlier <cedric.charlier@yucom.be>
2020-10-18 21:05:45 +0200

    Add PowerPoint presentation and scripts for demos

---
+++ download-github-history.ps1
@@ -0,0 +1,4 @@
+﻿$localStorage = "E:\Users\cedri\Downloads"
+For ($i=0; $i -le 23; $i++) {
+    Invoke-WebRequest -Uri "http://data.gharchive.org/2020-08-01-$i.json.gz" -OutFile "$localStorage\Github-2020-08-01-$i.json.gz"
+}
